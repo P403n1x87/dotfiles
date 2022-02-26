fd96e568f7d0c87dc72f790df697d3b4d8b6ce55
Gabriele N. Tornetta <phoenix1987@gmail.com>
2022-02-26 14:33:04 +0000

    Add DBUS configuration to spotify launcher

--- .spotify.sh
+++ .spotify.sh
@@ -3,6 +3,7 @@
 pkexec systemd-nspawn \
   --setenv=DISPLAY=unix$DISPLAY \
   --setenv=PULSE_SERVER=unix:/run/user/1000/pulse/native \
+  --setenv=DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus \
   --bind-ro /tmp/.X11-unix \
   --bind /run/user/1000 \
   --bind-ro /run/user/1000/bus \
